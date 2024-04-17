e7b668c2805982b875bf144de981d89ea4fd9f1c
ez337 <ezgaming3377@gmail.com>
2024-04-17 10:13:16 -0400

    Whoops I did setCover. Going to try Exact Cover

--- hw6/main.cpp
+++ hw6/main.cpp
@@ -6,11 +6,11 @@
 
 int main()
 {
-    auto start_time = time(nullptr);
     const int MAX_TIME = 4;
 
     int N, K, skillCount, skillID;
     std::vector<std::unordered_set<int>> people;
+    std::vector<int> included_People(N);
 
     std::cin >> N >> K;
 
@@ -25,30 +25,54 @@ int main()
         {
             std::cin >> skillID;
             people[i].insert(skillID);
+
         }
+
+        // Start off with everyone in the set
+        included_People.push_back(i);
+
     } // Data collection end
+
+    // Start time
+    auto start_time = time(nullptr);
 
     // Initialise the complete set
     std::unordered_set<int> final_Set;
-    std::vector<int> included_People;
+    std::vector<int> final_people(N); // Up to N people will be included
 
     // Start greedy algorithm. Only run for MAX_TIME before quitting and outputting
-    do
-    {
-        for (auto &elm : people)
-        {
-            std::unordered_set<int> tempSet;
-            std::set_union(elm.begin(), elm.end(), final_Set.begin(), final_Set.end(),
-                std::insert_iterator<std::unordered_set<int>>(tempSet, tempSet.begin()));
+    // This is the base. Now it needs optimisation
 
-            // We gained stuff
+    for (int i = 0; i < N; ++i)
+    {
+        auto x = (time(nullptr) - start_time);
+        std::cout << "Time is: " << x << std::endl;
+
+
+        if (time(nullptr) - start_time < MAX_TIME)
+        {
+            // Compute the number of uncovered elements by this person's set
+            std::unordered_set<int> tempSet;
+            std::set_union(people[i].begin(), people[i].end(), final_Set.begin(), final_Set.end(),
+                std::inserter(tempSet, tempSet.begin()));
+
+            // If this person's set covers more elements than the current maximum, update
             if (tempSet.size() > final_Set.size())
             {
-                if ()
+                std::swap(final_Set, tempSet); // Do it in constant time
+                final_people.push_back(i);
             }
         }
+        else if (final_Set.size() == K) // If we run out of time AND we got all of our elements,
+        {
+
+            std::swap(included_People, final_people);
+            break;
+        }
         
-    } while (time(nullptr) - start_time < MAX_TIME);
+
+    }
+
 
 
 
