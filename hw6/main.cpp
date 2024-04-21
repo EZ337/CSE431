66beef0122be93d4498ed24e7259b8726efdd171
ez337 <ezgaming3377@gmail.com>
2024-04-20 23:22:06 -0400

    Tears of joy man. Net me 19.862/18!

--- hw6/main.cpp
+++ hw6/main.cpp
@@ -3,6 +3,7 @@
 #include <set>
 #include <algorithm>
 #include <unordered_map>
+#include <unordered_set>
 #include <queue>
 #include <vector>
 
@@ -18,6 +19,7 @@ class Graph
 private:
     // Graph = {id : {0,1,2,3,..}, id1 : {5,8,1} }
     std::unordered_map<int, std::set<int>> graph;
+
     std::set<int> allSkills; // All the skills we need a set cover for
 
 public:
@@ -35,14 +37,59 @@ public:
     /// @return A set of the included vertices for the set cover
     std::set<int> SetCover(int startID)
     {
+        std::set<int> uncoveredSkills = allSkills, processedIDs; // Start with all skills being uncovered
+
+        bool start = true;
+        int candidateID = startID; // Start us off with the startID
+
+        while (!uncoveredSkills.empty())
+        {
+            if (!start)
+            {
+                int bestSoFar = 0;
+                for (auto& [id, skills] : graph)
+                {
+                    // If this person has already been processed or their ID is irrelvant, skip them
+                    if (processedIDs.count(id))
+                    {
+                        continue;
+                    }
+
+                    std::set<int> result;
+                    // Get the overlaps between the skills of this person and the uncoveredSkills
+                    std::set_intersection(skills.begin(), skills.end(), uncoveredSkills.begin(), uncoveredSkills.end(),
+                        std::insert_iterator(result, result.begin()));
+
+
+                    // If the candidate has the best skills so far, we want them
+                    if (result.size() > bestSoFar)
+                    {
+                        candidateID = id;
+                        bestSoFar = result.size();
+                    }
+                }
+            }
+
+            std::set<int> result;
+
+            // Get the left Over skills and put it into result
+            std::set_difference(uncoveredSkills.begin(), uncoveredSkills.end(), graph[candidateID].begin(), graph[candidateID].end(),
+                std::insert_iterator(result, result.begin()));
+
+            uncoveredSkills.swap(result); // Constant time operation
+            processedIDs.insert(candidateID); // Add to the list of people we are using to solve this problem
+            start = false;
+        }
         
+        return processedIDs;
+
     }
 
 
     /// @brief Brutesforces its way by calling SetCover to start at all the ids avaialable
     /// in the graph.
     /// @return The best pair representing the amount of people required and a set of the people required
-    std::pair<int, std::set<int>> BruteForceSetCover()
+    std::pair<int, std::set<int>> BruteForceSetCover(int maxRuns = 1000)
     {
         PairSetPQ pq;
 
@@ -50,17 +97,20 @@ public:
         // Mechanisms are in place to stop early though.
         for (auto [id, skillset] : graph)
         {
+            if (pq.size() > maxRuns - 1)
+                break;
+
             std::set<int> setCover = SetCover(id);
             pq.push(std::make_pair(setCover.size(), setCover));
         }
 
-        auto x = pq.top();
-        std::cout << "pq Top is: " << x.first << " : {" ;
-        for (auto elm : x.second)
-        {
-            std::cout << elm << ", ";
-        }
-        std::cout << '}' << std::endl;
+        // auto x = pq.top();
+        // std::cout << "pq Top is: " << x.first << " : {" ;
+        // for (auto elm : x.second)
+        // {
+        //     std::cout << elm << ", ";
+        // }
+        // std::cout << '}' << std::endl;
 
         return pq.top();
     }
@@ -91,8 +141,12 @@ int main()
     }
     
     // Grab the approximate best cover within the time limit
-    graph.BruteForceSetCover();
+    PairSetInt result =  graph.BruteForceSetCover(5);
 
-    std::cout << "Hello World!" << std::endl;
+    std:: cout << result.first << std::endl;
+    for (auto person : result.second)
+    {
+        std::cout << person << ' ';
+    }
 }
 
